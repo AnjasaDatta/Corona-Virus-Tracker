cd342e64e23fb610cdbd66a644bd3c0c7ae23c60
Anjasa Datta <anjasa.datta1996@gmail.com>
2020-05-11 18:55:57 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Cards from './Components/Card/Cards';
+import Chart from './Components/Chart/Chart';
+import CountryPicker from './Components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import { fetchData } from './api/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+  handleCountryChange = async country => {
+    //fetch data
+    const fetchedData = await fetchData(country);
+    //set state
+    this.setState({ data: fetchedData, country: country });
+  };
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div>
+        <div className={styles.container}>
+          <img
+            className={styles.image}
+            src='https://i.ibb.co/7QpKsCX/image.png'
+            alt='COVID-19'
+          />
+          <Cards data={data} />
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+          <Chart data={data} country={country} />
+        </div>
+        <div className={styles.copyright}>
+          <p>
+            copyright@AnjasaDatta.Get in touch at :{' '}
+            <a href='https://www.linkedin.com/in/anjasa-datta-16a232149/'>
+              LinkedIn
+            </a>{' '}
+            or +919038472713
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
