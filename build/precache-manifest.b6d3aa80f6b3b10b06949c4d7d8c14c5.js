4b947c722176fd250e4a259111c212507e733d1d
Anjasa Datta <anjasa.datta1996@gmail.com>
2020-05-11 19:57:40 +0530

    build

---
+++ build/precache-manifest.b6d3aa80f6b3b10b06949c4d7d8c14c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229fbca5277df69060aaae38f5a5481e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb83e8330c392f12df99",
+    "url": "/static/css/main.f547834c.chunk.css"
+  },
+  {
+    "revision": "bebf006860f2392a07c5",
+    "url": "/static/js/2.0e7ac82f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.0e7ac82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb83e8330c392f12df99",
+    "url": "/static/js/main.e075f3a2.chunk.js"
+  },
+  {
+    "revision": "e86fa83bcfb37cd3b1d3",
+    "url": "/static/js/runtime-main.ecf3b508.js"
+  }
+]);
